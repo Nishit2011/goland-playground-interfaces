7a2499468cd80b920e3e5fe870ab9f1faa4eaa46
Ranjan, Nishit (US - Bengaluru) <nishiranjan@deloitte.com>
2018-10-21 12:02:43 +0530

    Removing eb and sb as they are not being used. Refactoring the functions

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ func main() {
 
 //creating two bots to generate english and spanish greetings
 //the receiver value here is eb
-func (eb englishBot) getGreeting() string {
+func (englishBot) getGreeting() string {
 	//very custom logic for generating an english greeting
 	return "Hi There!"
 }
 
 //the receiver value here is sb
-func (sb spanishBot) getGreeting() string {
+func (spanishBot) getGreeting() string {
 	//very custom logic for generating an english greeting
 	return "Hola!"
 }
